aec1ddacf1c89d08130c8c05e9d6a5f5e95e35c7
Dan <dankroga@gmail.com>
2017-07-22 23:41:00 +0200

    ye its late, trying to add couple of things

--- downloadfiles.sh
+++ downloadfiles.sh
@@ -4,6 +4,33 @@
 #echo All files
 #ls -lh
 
+#================================================
+#Get local ip of http server from my other pc
+IPoutput=$( arp -a )
+echo ----------------------------
+echo LOCAL IP ADDRESSES:
+#echo $IPoutput
+#for str in $IPoutput
+#do
+#	echo $str
+#done
+echo FOR HTTPSERVER
+
+IPcut=$( echo $IPoutput | cut -d'(' -f 2 | cut -d')' -f 1 )
+echo $IPcut
+
+#==============================================
+echo Some curl
+curl -I "10.166.6.199:8000"
+#Now we download the files
+echo ---------------------------
+echo Downloading index.html
+BaseIPpath=$IPcut":8000/"
+wget "10.166.6.199:8000"
+
+
+
+
 #musze/trzeba html scrape index.html
 echo HTML content of index.html from http server
 cat index.html
@@ -33,30 +60,19 @@ done
 echo Loop done, saved to variable
 #echo $s
 
-#================================================
-#Get local ip of http server from my other pc
-IPoutput=$( arp -a )
-echo ----------------------------
-echo LOCAL IP ADDRESSES:
-#echo $IPoutput
-#for str in $IPoutput
-#do
-#	echo $str
-#done
-echo FOR HTTPSERVER
-
-IPcut=$( echo $IPoutput | cut -d'(' -f 2 | cut -d')' -f 1 )
-echo $IPcut
-
 #==============================================
 #Now we download the files
 echo ---------------------------
 echo Downloading files from HTTP server
-BaseIPpath=$IPcut":8000/"
+#BaseIPpath=$IPcut":8000/"
+BaseIPpath="10.166.6.199:8000/"
 for file in $s
 do
 	#gdy dodac string do inny, nie wkladam +.
 	#bezposrednio $string1$string2, a nie $string1+$string2
+	echo curl the files
+	curl -I $BaseIPpath$file
+	echo Download the file
 	wget $BaseIPpath$file
 done
 
